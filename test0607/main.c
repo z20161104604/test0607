348ed95c0affad1986e64b12762c50718aac91a7
z20161104604 <z20161104604@mac05.local>
2017-06-07 08:46:46 +0800

    Initial Commit

---
+++ test0607/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test0607
+//
+//  Created by z20161104604 on 17/6/7.
+//  Copyright © 2017年 z20161104604. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
